9a380a9c111582ebd4d498d1c8ec825568df89c1
Axel Moussard <a.moussard@novactive.com>
2013-02-27 23:19:57 -0500

    Chat

---
+++ js/chat.js
@@ -0,0 +1,62 @@
+(function($){
+
+    var msg = $('#msgtpl').html();
+    var lastmsg = false;
+    $('#msgtpl').remove();
+
+    // Connexion au serveur
+    var socket = io.connect('http://nodejs.test:8080');
+
+    $("#loginform").submit(function (event) {
+        event.preventDefault();
+        socket.emit('login', {
+            username: $('#username').val(),
+            mail: $('#mail').val()
+        });
+    });
+
+    /**
+     * Gestion des messages
+     */
+
+    $('#form').submit(function(event){
+        event.preventDefault();
+        socket.emit('newmsg', {message : $('#message').val() });
+        $('#message').val('');
+        $('#message').focus();
+        return false;
+    });
+
+    socket.on('newmsg', function(message){
+        if(lastmsg != message.user.id){
+            $('#messages').prepend('<div class="sep"></div>');
+            lastmsg = message.user.id;
+        }
+        $('#messages').prepend( '<div class="message">' + Mustache.render(msg, message) + '</div>' );
+        $("#messages").animate({ scrollTop: $("#messages").prop("scrollHeight") }, 500);
+    });
+
+    /**
+     * Gestion des connectés
+     */
+
+    socket.on('logged', function(){
+        $('#login').fadeOut();
+        $('#users, #messages, #form').show();
+        $('#message').focus();
+    });
+
+    socket.on('newusr', function(user){
+        console.log("remove " + user.id);
+        $('#'+user.id).remove();
+        $('#users').append('<img src="' + user.avatar + '" id="' + user.id + '">')
+    });
+
+    socket.on('disusr', function(user){
+        console.log("remove " + user.id);
+        $('#'+user.id).slideUp(100, function(){
+            $(this).remove();
+        })
+    })
+
+})(jQuery);
---
+++ server.js
@@ -0,0 +1,68 @@
+var http = require('http');
+var md5 = require('MD5');
+
+httpServer = http.createServer(function (request, response) {
+}).listen(8080, "nodejs.test");
+console.log('Server running at http://nodejs.test/');
+
+
+var io = require('socket.io').listen(httpServer) ;
+var users = {};
+var messages = [];
+var history = 10;
+
+io.sockets.on('connection', function (socket) {
+    var me = false;
+
+    for (var k in users) {
+        socket.emit('newusr', users[k]);
+    }
+    for (var k in messages) {
+        socket.emit('newmsg', messages[k]);
+    }
+
+    /**
+     * A la connexion
+     */
+    socket.on('login', function (user) {
+        me = user;
+        me.id = user.mail.replace(/@/g, '-').replace(/\./g, '-');
+        me.avatar = 'https://gravatar.com/avatar/' + md5(user.mail) + '?s=50';
+        users[me.id] = me;
+        console.log(users);
+        // Envoie un event à l'utilisateur concerné
+        socket.emit('logged');
+        // Envoie un event à tous sauf le concerné
+        // socket.broadcast.emit('newusr');
+        // Envoie un event à tous les sockets
+        io.sockets.emit('newusr', me);
+    });
+
+    /**
+     * A la déconnexion
+     */
+    socket.on('disconnect', function () {
+        if (!me) {
+            return false;
+        }
+
+        delete users[me.id];
+        io.sockets.emit('disusr', me);
+    });
+
+    /**
+     * Réception message
+     */
+    socket.on('newmsg', function (message) {
+        message.user = me;
+        date = new Date();
+        message.h = date.getHours();
+        message.m = date.getMinutes();
+        messages.push(message);
+        if (messages.length > history) {
+            messages.shift();
+        }
+        io.sockets.emit('newmsg', message);
+    });
+
+});
